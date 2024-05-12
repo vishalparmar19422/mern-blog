c1a69af3c90785f0abf885fb43f34ebb26f8ee8a
vishalparmar19422 <parmarvishal1942@gmail.com>
2024-05-12 18:17:14 +0530

    added home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,14 @@ import Signin from "./pages/signin";
 import Blog from "./pages/blog";
 import Fullblog from "./pages/fullBlog";
 import Writeblog from "./pages/writeBlog";
+import HomePage from "./pages/homepage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog" element={<Blog />} />
---
+++ frontend/src/pages/homepage.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="main">Hello this is home page go to /sign in if you are already a user if not go to /signup</div>
+        </>
+    )
+}   
+
+export default HomePage
